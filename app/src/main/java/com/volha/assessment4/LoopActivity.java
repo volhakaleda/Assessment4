1a76c0bb0e38690c6c76602eee5f15d46c80c32c
volha <Volhakaleda@ac.c4q.nyc>
2017-12-06 19:05:40 -0500

    initial commit

---
+++ app/src/main/java/com/volha/assessment4/LoopActivity.java
@@ -0,0 +1,13 @@
+package com.volha.assessment4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoopActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_loop);
+  }
+}
