2a8bf30ffafa37dd0c167cfcdc1dc00f5c9acce8
volha <Volhakaleda@ac.c4q.nyc>
2017-12-06 19:30:25 -0500

    added part01

--- app/src/main/java/com/volha/assessment4/LoopActivity.java
+++ app/src/main/java/com/volha/assessment4/LoopActivity.java
@@ -2,6 +2,7 @@ package com.volha.assessment4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class LoopActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class LoopActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_loop);
+
+    TextView textView = findViewById(R.id.text);
+
+    CustomAsync customAsync = new CustomAsync(textView, this);
+    customAsync.execute(100);
   }
 }
